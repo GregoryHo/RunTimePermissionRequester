460e01de947b757c0b0dcf330d1ea7fd1c7b7479
Gregory <greghojob@gmail.com>
2017-10-02 11:05:45 +0800

    Change inheritance to Activity

--- rt-permissionrequester/src/main/java/com/ns/greg/library/rt_permissionrequester/PermissionRequestActivity.java
+++ rt-permissionrequester/src/main/java/com/ns/greg/library/rt_permissionrequester/PermissionRequestActivity.java
@@ -1,19 +1,20 @@
 package com.ns.greg.library.rt_permissionrequester;
 
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import com.ns.greg.library.rt_permissionrequester.module.RequestingPermission;
 
 /**
  * @author Gregory
  * @since 2017/6/30
  */
-public abstract class PermissionRequestActivity extends AppCompatActivity {
+public abstract class PermissionRequestActivity extends Activity {
 
   public static final int PERMISSION_REQUEST_CODE = 0xBC;
 
-  protected abstract void onRequest(@RequestingPermission.RequestPermission String permission, boolean granted);
+  protected abstract void onRequest(@RequestingPermission.RequestPermission String permission,
+      boolean granted);
 
   @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
       @NonNull int[] grantResults) {
@@ -24,6 +25,9 @@ public abstract class PermissionRequestActivity extends AppCompatActivity {
           onRequest(permissions[i], grantResults[i] == PackageManager.PERMISSION_GRANTED);
         }
 
+        break;
+
+      default:
         break;
     }
   }
